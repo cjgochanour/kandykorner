7000a1d1cb4a81af02beff1dfe7de856cfc26576
cjgochanour <cjgochanour@gmail.com>
2022-01-12 15:59:37 -0600

    added user authentication

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,15 +1,34 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { NavBar } from "./nav/NavBar.js";
 import { ApplicationViews } from "./ApplicationViews.js";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
 export const KandyKorner = () => {
     return (
         <>
-            <Route>
-                <NavBar />
-                <h1>Kandy Korner</h1>
-                <ApplicationViews />
+            <Route
+                render={() => {
+                    if (localStorage.getItem("kandy_customer")) {
+                        return (
+                            <>
+                                <NavBar />
+                                <h1>Kandy Korner</h1>
+                                <ApplicationViews />
+                            </>
+                        );
+                    } else {
+                        return <Redirect to="/login" />;
+                    }
+                }}
+            />
+
+            <Route path="/login">
+                <Login />
+            </Route>
+            <Route path="/register">
+                <Register />
             </Route>
         </>
     );
